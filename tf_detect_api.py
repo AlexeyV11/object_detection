94f0e7a0ad0d530ab415e63fe2b71a2dd783b57d
Alexey <menamet31@gmail.com>
2018-10-21 15:22:49 +0100

    original sample refactored

--- tf_detect_api.py
+++ tf_detect_api.py
@@ -1,3 +1,7 @@
+# add pathes from TF repository
+sys.path.append("../models/research")
+sys.path.append("../models/research/cognitive_planning")
+
 import numpy as np
 import os
 import six.moves.urllib as urllib
@@ -12,57 +16,32 @@ from io import StringIO
 from matplotlib import pyplot as plt
 from PIL import Image
 
-# This is needed since the notebook is stored in the object_detection folder.
-sys.path.append("../models/research")
-sys.path.append("../models/research/cognitive_planning")
+
+# TF Object detection imports
 
 from object_detection.utils import ops as utils_ops
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
 
 if StrictVersion(tf.__version__) < StrictVersion('1.9.0'):
     raise ImportError('Please upgrade your TensorFlow installation to v1.9.* or later!')
 
-# Object detection imports
-from object_detection.utils import label_map_util
-from object_detection.utils import visualization_utils as vis_util
 
-# Model preparation
+def download_model(model_name):
+    model_file = model_name + '.tar.gz'
+    download_base = 'http://download.tensorflow.org/models/object_detection/'
 
-MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
-MODEL_FILE = MODEL_NAME + '.tar.gz'
-DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
-
-# Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
-
-# List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.path.join('..', 'models', 'research', 'object_detection', 'data', 'mscoco_label_map.pbtxt')
-
-
-if not os.path.exists(MODEL_NAME):
-    # Download Model
-    opener = urllib.request.URLopener()
-    opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-    tar_file = tarfile.open(MODEL_FILE)
-    for file in tar_file.getmembers():
-        file_name = os.path.basename(file.name)
-        if 'frozen_inference_graph.pb' in file_name:
-            tar_file.extract(file, os.getcwd())
-    os.remove(MODEL_FILE)
-
-# Load a (frozen) Tensorflow model into memory.¶
-detection_graph = tf.Graph()
-with detection_graph.as_default():
-    od_graph_def = tf.GraphDef()
-    with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
-        serialized_graph = fid.read()
-        od_graph_def.ParseFromString(serialized_graph)
-        tf.import_graph_def(od_graph_def, name='')
-
-# Loading label map
-# Label maps map indices to category names, so that when our convolution network predicts 5, we know that this corresponds to airplane.
-# Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
-
-category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+    if not os.path.exists(model_name):
+        # Download Model
+        opener = urllib.request.URLopener()
+        opener.retrieve(download_base + model_file, model_file)
+        tar_file = tarfile.open(model_file)
+        for file in tar_file.getmembers():
+            file_name = os.path.basename(file.name)
+            if 'frozen_inference_graph.pb' in file_name:
+                tar_file.extract(file, os.getcwd())
+        os.remove(model_file)
 
 
 # Helper code¶
@@ -70,17 +49,6 @@ def load_image_into_numpy_array(image):
     (im_width, im_height) = image.size
     return np.array(image.getdata()).reshape(
         (im_height, im_width, 3)).astype(np.uint8)
-
-
-# For the sake of simplicity we will use only 2 images:
-# image1.jpg
-# image2.jpg
-# If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
-PATH_TO_TEST_IMAGES_DIR = 'images'
-TEST_IMAGE_PATHS = [os.path.join(PATH_TO_TEST_IMAGES_DIR, i) for i in ["cars.jpg", "pedestrians.jpg"]]
-
-# Size, in inches, of the output images.
-IMAGE_SIZE = (12, 8)
 
 
 def run_inference_for_single_image(image, graph):
@@ -130,25 +98,63 @@ def run_inference_for_single_image(image, graph):
     return output_dict
 
 
-for image_path in TEST_IMAGE_PATHS:
-    image = Image.open(image_path)
-    # the array based representation of the image will be used later in order to prepare the
-    # result image with boxes and labels on it.
-    image_np = load_image_into_numpy_array(image)
-    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-    image_np_expanded = np.expand_dims(image_np, axis=0)
-    # Actual detection.
-    output_dict = run_inference_for_single_image(image_np, detection_graph)
-    # Visualization of the results of a detection.
-    vis_util.visualize_boxes_and_labels_on_image_array(
-        image_np,
-        output_dict['detection_boxes'],
-        output_dict['detection_classes'],
-        output_dict['detection_scores'],
-        category_index,
-        instance_masks=output_dict.get('detection_masks'),
-        use_normalized_coordinates=True,
-        line_thickness=8)
-    plt.figure(figsize=IMAGE_SIZE)
-    plt.imshow(image_np)
-    plt.waitforbuttonpress()
+def main():
+    # Model name
+    MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+
+    # Path to frozen detection graph. This is the actual model that is used for the object detection.
+    PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
+
+    # Path to coco labels.
+    PATH_TO_LABELS = os.path.join('..', 'models', 'research', 'object_detection', 'data', 'mscoco_label_map.pbtxt')
+
+    # path to folder with images; we work with jpg's
+    PATH_TO_TEST_IMAGES_DIR = 'images'
+
+    # Size, in inches, of the output images.
+    IMAGE_SIZE = (12, 8)
+
+    # Loading label map
+    # Label maps map indices to category names, so that when our convolution network predicts 5, we know that this corresponds to airplane.
+    # Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+    category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+    download_model(MODEL_NAME)
+
+    # Load a (frozen) Tensorflow model into memory.¶
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+
+    test_images_paths = [os.path.join(PATH_TO_TEST_IMAGES_DIR, i) for i in os.listdir(PATH_TO_TEST_IMAGES_DIR)]
+
+    for image_path in test_images_paths:
+        image = Image.open(image_path)
+        # the array based representation of the image will be used later in order to prepare the
+        # result image with boxes and labels on it.
+        image_np = load_image_into_numpy_array(image)
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        # Actual detection.
+        output_dict = run_inference_for_single_image(image_np, detection_graph)
+        # Visualization of the results of a detection.
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            image_np,
+            output_dict['detection_boxes'],
+            output_dict['detection_classes'],
+            output_dict['detection_scores'],
+            category_index,
+            instance_masks=output_dict.get('detection_masks'),
+            use_normalized_coordinates=True,
+            line_thickness=8)
+        plt.figure(figsize=IMAGE_SIZE)
+        plt.imshow(image_np)
+        plt.waitforbuttonpress()
+
+
+if __name__ == "__main__":
+    main()
